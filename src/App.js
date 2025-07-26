e2f7d2d271906019618dcbc9eddd619ea63b7da0
Francesco Carlesi <tuaemail@esempio.com>
2025-07-26 20:29:43 +0200

    feat: Struttura completa progetto React con Tailwind CSS
    - Aggiunto package.json con tutte le dipendenze
    - Creata struttura cartelle src e public
    - Configurato Tailwind CSS
    - App pronta per npm start su porta 3000

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExcelLikeBankPlan from './BankPlan';
+
+function App() {
+  return (
+    <div className="App">
+      <ExcelLikeBankPlan />
+    </div>
+  );
+}
+
+export default App;
